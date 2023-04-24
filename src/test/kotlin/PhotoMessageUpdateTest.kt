7a6fa4315bf249c2a400b9336f07a013190d83e4
dim4 <dmitrvls@gmail.com>
2023-04-24 12:45:19 +0600

    0.4.1 Tested photo message update feature

--- src/test/kotlin/PhotoMessageUpdateTest.kt
+++ src/test/kotlin/PhotoMessageUpdateTest.kt
@@ -1,23 +1,31 @@
 import dev.inmo.tgbotapi.extensions.api.edit.caption.editMessageCaption
+import dev.inmo.tgbotapi.extensions.api.edit.media.editMessageMedia
 import dev.inmo.tgbotapi.extensions.api.send.media.sendPhoto
 import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
 import dev.inmo.tgbotapi.requests.abstracts.InputFile
 import dev.inmo.tgbotapi.types.ChatId
+import dev.inmo.tgbotapi.types.media.TelegramMediaPhoto
 import io.kotest.core.spec.style.StringSpec
 
 class PhotoMessageUpdateTest : StringSpec({
     "Edit photo caption" {
         val id = ChatId(chatId = 509933088)
         bot.buildBehaviourWithLongPolling {
-            val message = bot.sendPhoto(id, InputFile.fromUrl("https://yt3.googleusercontent.com/umigHrIECcGghXOyOb73g0gGPbV164TDoL5xQ5j8RE-O84eXpHW8SDn3eBZDSiloZWnNRU5Br4Q=s900-c-k-c0x00ffffff-no-rj"))
+            val message = bot.sendPhoto(
+                id,
+                InputFile.fromUrl("https://yt3.googleusercontent.com/umigHrIECcGghXOyOb73g0gGPbV164TDoL5xQ5j8RE-O84eXpHW8SDn3eBZDSiloZWnNRU5Br4Q=s900-c-k-c0x00ffffff-no-rj")
+            )
             editMessageCaption(id, message.messageId, "caption")
         }
     }
     "Replace photo" {
         val id = ChatId(chatId = 509933088)
         bot.buildBehaviourWithLongPolling {
-            val message = bot.sendPhoto(id, InputFile.fromUrl("https://yt3.googleusercontent.com/umigHrIECcGghXOyOb73g0gGPbV164TDoL5xQ5j8RE-O84eXpHW8SDn3eBZDSiloZWnNRU5Br4Q=s900-c-k-c0x00ffffff-no-rj"))
-            //editMessageMedia(chatId = id, message.messageId, )
+            val message = bot.sendPhoto(
+                id,
+                InputFile.fromUrl("https://yt3.googleusercontent.com/umigHrIECcGghXOyOb73g0gGPbV164TDoL5xQ5j8RE-O84eXpHW8SDn3eBZDSiloZWnNRU5Br4Q=s900-c-k-c0x00ffffff-no-rj")
+            )
+            bot.editMessageMedia(message=message, media = TelegramMediaPhoto(file = InputFile.fromUrl("https://i1.sndcdn.com/artworks-CyTzk0PMsjHFfr7D-S8wWcw-t500x500.jpg")))
         }
     }
 })
